96c4c567b0c490204c3d0d07918c847d91e4ea5f
Kozlows <szymon.p.kozlowski@gmail.com>
2024-09-01 17:55:05 +0100

    Finalised Card and Deck class

--- blackjack.cpp
+++ blackjack.cpp
@@ -7,75 +7,135 @@ class Card{  // Class for the cards
 
     public:
         int worth; // Init for cards worth, for ex. a 5 has a worth of 5, a J (Jack) has a worth of 10
+
+        Card(){  // Default Card init for creating placeholders for a card, for arrays or other reasons
+            this->card = "Empty";
+            this-> worth = 0;
+        }
+
         Card(std::string card, int worth){  //Init function for the Card class, asks for card name and cards worth as input
             this-> card = card;  // Sets the cards name
             this-> worth = worth;  // Sets the cards worth
         }
 
-        void printName(){  // Prints the name of the card, will be replaced by the __str__() equivilant once I learn how it works
-            std::cout << card;
+        void printCard(){  // Prints the name and worth of the card, will be replaced by the __str__() equivilant once I learn how it works
+            std::cout << card << "; " << worth << '\n';
         }
 };
-class Deck{  // Class for the deck, currently does not work
+class Deck{  // Class for the deck
     private:
         int valuesSize = 13;  // Number of Values
         int suitsSize = 4;  // Number of Suits
-        std::string values[13] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};  // Gives me an error when i input the above variable instead of the 13
+        std::string values[13] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};  // Gives me an error when I input the above variable instead of the 13
         std::string suits[4] = {"S", "H", "D", "C"};  // Same problem as above, if someone finds a nicer/cleaner solution for this feel free to message me about it
-        Card deck[52];  // Is meant to init an array for the cards of size 52, however i believe this is actually causing me problems and why the deck class does not work rn
-
-        // I dont believe the compiler is smart enough to realise that it can store an empty Card, as Card init requires 2 inputs. This is prob the reason why deck class does not work
-        // Overloading might solve this issue, ill work on it when I wake up
+        Card *pDeck = NULL;  // Creates a pointer to a card class variable, this will be a Card array storing every card using dynamic memory
+        int deckSize = valuesSize * suitsSize;  // Creates a variable which will mention the deck size, being 52 on init
 
         int cardWorth(std::string name){  // Method for calculating a cards value based on its name
             for(int i = 0; i < 9; i++){  // If the names value is between A-9, then its worth is just the index + 1, obv not 100% true for aces, however not nec to calc rn
                 if(name.at(0) + values[i] == name){
                     return i + 1;
                 }
-                return 10;  // Otherwise, its value is 10
             }
+            return 10;  // Otherwise, its value is 10
         };
-    public:
-        void initDeck(){  // Method for initialising the Deck, rn below code simply addes every possible card to deck. Randomness will be added later. Does not work yet because of issue mentioned above
-            for(int i = 0; i < suitsSize; i++){
-                for(int j = 0; j < valuesSize; j++){
-                    std::string name = suits[i] + values[j];
-                    int worth = cardWorth(name);
-                    Card card(suits[i] + values[j], worth);
-                    deck[(i * valuesSize) + j] = card;  // Not entierly sure this works, will need to confirm after problem from above gets fixed
+
+        void createDeck(int amountOfDecks){  // Creates a deck with a specified amount of standard decks, without shuffling
+            for(int i = 0; i < amountOfDecks; i++){
+                for(int j = 0; j < suitsSize; j++){
+                    for(int k = 0; k < valuesSize; k++){
+                        std::string name = suits[j] + values[k];  // Creates a variable name combining the suit and value
+                        int worth = cardWorth(name);  // Calculates the cards worth
+                        Card card(name, worth);  // Initialises the card with the previously found name and worth
+                        pDeck[(i * valuesSize * suitsSize) + (j * valuesSize) + k] = card;  // Adds card to the deck
+                    }
                 }
             }
         }
 
-        void printCard(int index){  // Prints the name of card at specified index of deck. For debugging purposes
-            deck[index].printName();
+        void shuffleDeck(){  // Using the Fisher–Yates shuffle Algorithm, O(n^2), shuffles the deck
+            for(int i = deckSize - 1; i > 0; i--){  // Goes through every index in the deck
+                int j = rand() % (i + 1);  // Gets a random value between 0 and the current i
+                Card tmp = pDeck[i];
+                pDeck[i] = pDeck[j];
+                pDeck[j] = tmp;
+                // Switches the cards at index i and j
+            }
+        }
+
+    public:
+
+        void deleteDeck(){  // Method to be called when finished playing blackjack, as pDeck uses dynamic memory
+            delete[] pDeck;
+        }
+
+        void printDeck(){  // Prints every card in the deck. Will be replaced with __str__ eqiv. once i learn how thats done
+            for(int i = 0; i < deckSize; i++){
+                pDeck[i].printCard();
+            }
+        }
+
+        
+
+        Deck(){  // Default init for deck incase I will need a placeholder for setting up an array
+
+        }
+
+        Deck(int amountOfDecks){  // Deck init asking for the amount of standard decks to be used for this deck
+            this->deckSize *= amountOfDecks;  // Updates deck size
+            pDeck = new Card[amountOfDecks * 52];  // Sets the deck variable into a Card variable using dynamic memory
+            createDeck(amountOfDecks);  // Adds actual cards to the deck
+            shuffleDeck();  // Shuffles the deck
         }
 };
-/*
+
+// The below Human, Dealer and Player classes might be modified/removed, depending on how I decide to go through with it
+
 class Human{  // Not yet implemented. Will store everything important to both the players and the dealer
+    private:
+        Card held[22];  // Creates a Card array of cards that will the human will hold. The reason for the 22 placeholders is that that is the max number of cards you would need for a bust
+        int minPoints = 0;  // Will store the minimum amount of points held
+        int maxPoints = 0;  // Will store the maximum amount of points held
+    /*
+    // Below methods might be removed/moved to a different class
+    public:
+        void hit(){
+
+        }
+
+        void stand(){
+
+        }
+
+        void split(){
+
+        }
+
+        void doubleDown(){
+
+        }
+    */
+};
+
+/*
+class Dealer : private Human{  // Not yet implemented. Will store everything important to the dealer
+};
+/*
+class Player : private Human{  // Not yet implemented. Will store everything important to the players
 
 };
-class Dealer : public Human{  // Not yet implemented. Will store everything important to the dealer
-
-};
-class Player : public Human{  // Not yet implemented. Will store everything important to the players
+class Blackjack{  // Not yet implemented. Will be the main program for the game
 
 };
 */
 
 int main() {
-    // Below Card code works fine, the Card class seems to have been set up well
-    Card card("HA", 1);
-    card.printName();
-    std::cout << " " << card.worth;
+    srand(time(NULL));  // Creates random seed based on time
 
-    // Below deck code does not work, whether there exists an init for the Deck or not. Error message does not really give too much of an insight into problem either
-    // Except that the Card class is in some way related, which is why I believe line 25 to be the problematic one.
 
-    /*
-    Deck deck;  // Calling this did not work no matter what I tried, will try overloading Card class next time I work on this to see if it helps
-    deck.initDeck();  // Will be moved into Decks init once problem from above is fixed
-    deck.printCard(1);  // Simply put here for debuggging purposes
-    return 0;
-    */
+    Deck deck(3);  // Creates a deck the size of 3 standard decks, 3 * 52, and shuffles them
+    deck.printDeck();  // Prints every card in this deck
+
+
+    deck.deleteDeck();  // Deletes dynamic memory usage
 }
