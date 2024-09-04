a81717c8faa349bae8cd7aad5ce5ea7bfd0d26c7
Kozlows <szymon.p.kozlowski@gmail.com>
2024-09-04 15:15:59 +0100

    Small Update

--- blackjack.cpp
+++ blackjack.cpp
@@ -18,18 +18,24 @@ class Card{  // Class for the cards
             this-> worth = worth;  // Sets the cards worth
         }
 
+        bool isEmpty(){
+            return worth == 0;
+        }
+
         void printCard(){  // Prints the name and worth of the card, will be replaced by the __str__() equivilant once I learn how it works
             std::cout << card << "; " << worth << '\n';
         }
 };
 class Deck{  // Class for the deck
     private:
-        int valuesSize = 13;  // Number of Values
-        int suitsSize = 4;  // Number of Suits
-        std::string values[13] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};  // Gives me an error when I input the above variable instead of the 13
-        std::string suits[4] = {"S", "H", "D", "C"};  // Same problem as above, if someone finds a nicer/cleaner solution for this feel free to message me about it
-        Card *pDeck = NULL;  // Creates a pointer to a card class variable, this will be a Card array storing every card using dynamic memory
-        int deckSize = valuesSize * suitsSize;  // Creates a variable which will mention the deck size, being 52 on init
+        static const int valuesSize = 13;  // Number of Values
+        static const int suitsSize = 4;  // Number of Suits
+        static const int deckAmount = 6;
+        static const int standardDeckSize = valuesSize * suitsSize;
+        static const int deckSize = deckAmount * standardDeckSize;
+        std::string values[valuesSize] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};  // Gives me an error when I input the above variable instead of the 13
+        std::string suits[suitsSize] = {"S", "H", "D", "C"};  // Same problem as above, if someone finds a nicer/cleaner solution for this feel free to message me about it
+        Card deck[deckSize]; 
 
         int cardWorth(std::string name){  // Method for calculating a cards value based on its name
             for(int i = 0; i < 9; i++){  // If the names value is between A-9, then its worth is just the index + 1, obv not 100% true for aces, however not nec to calc rn
@@ -40,52 +46,40 @@ class Deck{  // Class for the deck
             return 10;  // Otherwise, its value is 10
         };
 
-        void createDeck(int amountOfDecks){  // Creates a deck with a specified amount of standard decks, without shuffling
-            for(int i = 0; i < amountOfDecks; i++){
+        void createDeck(){  // Creates a deck with a specified amount of standard decks, without shuffling
+            for(int i = 0; i < deckAmount; i++){
                 for(int j = 0; j < suitsSize; j++){
                     for(int k = 0; k < valuesSize; k++){
                         std::string name = suits[j] + values[k];  // Creates a variable name combining the suit and value
                         int worth = cardWorth(name);  // Calculates the cards worth
                         Card card(name, worth);  // Initialises the card with the previously found name and worth
-                        pDeck[(i * valuesSize * suitsSize) + (j * valuesSize) + k] = card;  // Adds card to the deck
+                        deck[(i * valuesSize * suitsSize) + (j * valuesSize) + k] = card;  // Adds card to the deck
                     }
                 }
             }
+            shuffleDeck();
         }
 
         void shuffleDeck(){  // Using the Fisher–Yates shuffle Algorithm, O(n^2), shuffles the deck
             for(int i = deckSize - 1; i > 0; i--){  // Goes through every index in the deck
                 int j = rand() % (i + 1);  // Gets a random value between 0 and the current i
-                Card tmp = pDeck[i];
-                pDeck[i] = pDeck[j];
-                pDeck[j] = tmp;
+                Card tmp = deck[i];
+                deck[i] = deck[j];
+                deck[j] = tmp;
                 // Switches the cards at index i and j
             }
         }
 
     public:
 
-        void deleteDeck(){  // Method to be called when finished playing blackjack, as pDeck uses dynamic memory
-            delete[] pDeck;
-        }
-
         void printDeck(){  // Prints every card in the deck. Will be replaced with __str__ eqiv. once i learn how thats done
             for(int i = 0; i < deckSize; i++){
-                pDeck[i].printCard();
+                deck[i].printCard();
             }
         }
 
-        
-
-        Deck(){  // Default init for deck incase I will need a placeholder for setting up an array
-
-        }
-
-        Deck(int amountOfDecks){  // Deck init asking for the amount of standard decks to be used for this deck
-            this->deckSize *= amountOfDecks;  // Updates deck size
-            pDeck = new Card[amountOfDecks * 52];  // Sets the deck variable into a Card variable using dynamic memory
-            createDeck(amountOfDecks);  // Adds actual cards to the deck
-            shuffleDeck();  // Shuffles the deck
+        Deck(){  // Deck init asking for the amount of standard decks to be used for this deck
+            createDeck();  // Adds actual cards to the deck
         }
 };
 
@@ -94,48 +88,211 @@ class Deck{  // Class for the deck
 class Human{  // Not yet implemented. Will store everything important to both the players and the dealer
     private:
         Card held[22];  // Creates a Card array of cards that will the human will hold. The reason for the 22 placeholders is that that is the max number of cards you would need for a bust
-        int minPoints = 0;  // Will store the minimum amount of points held
-        int maxPoints = 0;  // Will store the maximum amount of points held
-    /*
-    // Below methods might be removed/moved to a different class
+        int minimumPoints;  // Will store the minimum amount of points held
+        int maximumPoints;  // Will store the maximum amount of points held
+    
     public:
-        void hit(){
-
+        bool bust(int points){
+            return points > 21;
+        }
+        
+        int maxPoints(){
+            return maximumPoints;
         }
 
-        void stand(){
-
+        int minPoints(){
+            return minimumPoints;
         }
 
-        void split(){
+
+        void calculatePoints(){
+            int min = 0;
+            bool hasAce = false;
+            for(Card card : held){
+                min += card.worth;
+                if (!hasAce and card.worth == 1){
+                    hasAce = true;
+                }
+            }
+            int max = min;
+            if (hasAce and !bust(max + 10)){
+                max += 10;
+            }
+            this-> minimumPoints = min;
+            this-> maximumPoints = max;
+        }
+        
+
+        Human(){
 
         }
-
-        void doubleDown(){
-
-        }
-    */
 };
 
-/*
+
 class Dealer : private Human{  // Not yet implemented. Will store everything important to the dealer
+    private:
+
+    public:
+        Dealer(){
+
+        }
+
+        bool isNatural(){
+            return maxPoints() == 21;
+        }
 };
-/*
+
 class Player : private Human{  // Not yet implemented. Will store everything important to the players
+    private:
+        std::string name;
+        int money;
+        int currentBet = 0;
+    
+    public:
+        Player(){
+
+        }
+
+        Player(std::string name, int money){
+            this->name = name;
+            this->money = money;
+        }
+
+        void setBet(int bet){
+            this->currentBet = bet;
+        }
+
+        int moneyCount(){
+            return money;
+        }
+
+        void printPlayer(){
+            std::cout << "Name: " << name << "\n" << "Money: €" << money << "\n";
+        }
+};
+
+class BlackJack{
+    private:
+        Dealer dealer;
+        Deck deck;
+        Player player;
+        int turn = 0;
+    public:
+
+    BlackJack(){
+        this-> player = askInfo();
+        do
+        {
+            playRound();
+        } while (askIfAnother());
+    }
+
+    void playRound(){
+        player.setBet(bet());
+        /*
+        draw();
+        if (dealer.isNatural()){
+            insurance();
+        }
+        else{
+            player.plays();
+            dealer.plays();
+        }
+        finishRound();
+        */
+    }
+
+    int bet(){
+        std::string betToMake;
+        int iBet;
+        std::string sBet;
+        do
+        {
+            do
+            {
+                std::cout << "Current Wallet: €" << player.moneyCount() << "\n";
+                std::cout << "How much do you wish to bet? (Must be a Natural Number): ";
+                std::getline(std::cin, betToMake);
+            } while (!isDigit(betToMake));
+            iBet = std::atoi(betToMake.c_str());
+            if (iBet > player.moneyCount()){
+                iBet = player.moneyCount();
+            }
+            sBet = std::to_string(iBet);
+        } while (!confirm("Are you sure you want to bet €" + sBet + "? (y/n): "));
+
+        return iBet;
+    }
+
+    std::string repeat(std::string str, int amount){
+        std::string finalStr = "";
+        for (int i = 0; i < amount; i++){
+            finalStr += str;
+        }
+        return finalStr;
+    }
+
+    bool askIfAnother(){
+        std::string text = "Do you wish to play another round? (y/n): ";
+        return confirm(text);
+    }
+
+    Player askInfo(){
+        bool askAgain;
+        std::cout << repeat("*", 10) << "\n";
+        std::string name;
+        std::string sMoney;
+        int money;
+        do
+        {
+            askAgain = false;
+            std::cout << "What is your name?: ";
+            std::getline(std::cin, name);
+            do
+            {
+                std::cout << "What is your buy in? (Natural Numbers Only): ";
+                std::getline(std::cin, sMoney);
+            } while (!isDigit(sMoney));
+
+            money = std::atoi(sMoney.c_str());
+            sMoney = std::to_string(money);
+
+            std::string text = "Name: " + name + "\n" + "Money: €" + sMoney + "\n" + "Is this correct? (y/n)" + "\n";
+
+            if(!confirm(text)){
+                askAgain = true;
+            }
+
+        } while (askAgain);
+
+
+        return Player(name, money);
+    }
+
+    bool confirm(std::string text){
+        std::string response;
+        do
+        {
+            std::cout << text;
+            std::getline(std::cin, response);
+        } while (response != "y" && response != "n");
+        return response == "y";
+    }
+
+    bool isDigit(std::string number){
+        for (int i = 0; i < number.length(); i++){
+            if (number.at(i) < '0' || number.at(i) > '9'){
+                return false;
+            }
+        }
+        return true;
+    }
 
 };
-class Blackjack{  // Not yet implemented. Will be the main program for the game
-
-};
-*/
 
 int main() {
     srand(time(NULL));  // Creates random seed based on time
 
 
-    Deck deck(3);  // Creates a deck the size of 3 standard decks, 3 * 52, and shuffles them
-    deck.printDeck();  // Prints every card in this deck
-
-
-    deck.deleteDeck();  // Deletes dynamic memory usage
+    BlackJack bj;
 }
